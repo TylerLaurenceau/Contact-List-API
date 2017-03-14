5d30dfd01f4f14040b30c9370e06fbee621e2c46
Tyler Laurenceau <TylerSLaurenceau@gmail.com>
2017-03-14 13:53:22 -0400

    added cors to my routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 // Require your controllers here
 const ContactController = require("../controllers/contact")
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
+
 module.exports = (app) => {
   // Add your routes here
   app.get('/contacts/',ContactController.findAll);
